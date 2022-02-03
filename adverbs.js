c55bbaad4c275236203847ad3e20babe43c7fef2
Blake Zucker <Zucker_Blake@bah.com>
2022-02-02 19:53:37 -0600

    add vocab + remove answered

--- adverbs.js
+++ adverbs.js
@@ -1,35 +1,86 @@
-/*  매우 (maeu)	very	
+const fs = require('fs')
 
- 아주 (aju)	very	 
+const vocab = `아주
+aju
+very
+자주
+jaju
+frequently
+정말
+jeongmal
+really
+조금
+jogeum
+a little
+꽤
+kkwae
+quite
+상당히
+sangdanghi
+quite
+거의
+geoui
+almost
+빨리
+ppalli
+fast
+천천히
+cheoncheonhi
+slowly
+신중하게
+sinjunghage
+carefully
+항상
+hangsang
+always
+늘
+neul
+always
+보통
+botong
+usually
+때로는
+ttaeroneun
+sometimes
+가끔
+gakkeum
+occasionally
+좀처럼
+jomcheoreom
+seldom
+드물게
+deumulge
+rarely
+절대
+jeoldae
+never
+만나서 반가워요
+Mannaseo bangawoeyo
+Nice to meet you
+어떻게 지내세요?
+Eotteoke jinaeseyo? 
+How are you?
+잘 지내요 
+Jal jinaeyo
+I'm great
+저는 커피를 마시어요
+naneun keopileul masieoyo
+I drink coffee
+나는 여기에 자주 오요
+naneun yeogie jaju oyo
+I come here often`
 
- 꽤(kkwae)	quite	 
+let array = vocab.split('\n');
+let words = []
 
- 상당히(sangdanghi)	quite	 
+for (let index = 0; index < array.length - 2; index = index + 3) {
+	
+	const obj = {};
+	obj.hangul = array[index]
+	obj.romaji = array[index + 1]
+	obj.def = array[index + 2]
+	words.push(obj)
+	
+}
 
- 거의(geoui)	almost	 
-
- 빨리 (ppalli)	fast	 
-
- 천천히 (cheoncheonhi)	slowly	 
- 신중하게 (sinjunghage)	carefully	 
-
- 항상 (hangsang)	always	 
-
- 늘(neul)	always	 
-
- 자주 (jaju)	frequently	 
-
- 흔히(heunhi)	frequently	 
- 보통 (botong)	usually	 
-
- 가끔 (gakkeum)	sometimes	 
-
- 때로는(ttaeroneun)	sometimes	 
- 가끔(gakkeum)	occasionally	
- 좀처럼(jomcheoreom )	seldom	 
-
- 드물게 (deumulge)	rarely	 
-
- 결코 (gyeolko)	never
-
- 절대 (jeoldae)	never	 */
+fs.writeFile('./output.js', JSON.stringify(words), { flag: 'a+' }, err => {})
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const vocab = [
+const primaryVocab = [
 	{
 		hangul: '가다',
 		romaji: 'gada',
@@ -9,11 +9,11 @@ const vocab = [
 		romaji: 'johahada',
 		def: 'to like'
 	},
-	/* 	{
+	{
 		hangul: '오다',
 		romaji: 'oda',
 		def: 'to come'
-	}, */
+	},
 	{
 		hangul: '있다',
 		romaji: 'itda',
@@ -22,7 +22,7 @@ const vocab = [
 	{
 		hangul: '이다',
 		romaji: 'ida',
-		def: 'to be '
+		def: 'to be'
 	},
 	{
 		hangul: '먹다',
@@ -54,16 +54,11 @@ const vocab = [
 		romaji: 'ireonada',
 		def: 'to wake up or get up'
 	},
-	{
-		hangul: '사다',
-		romaji: 'sada',
-		def: 'to buy'
-	},
-	{
+/* 	{
 		hangul: '쓰다',
 		romaji: 'sseuda',
 		def: 'to write or to use'
-	},
+	}, */
 	{
 		hangul: '웃다',
 		romaji: 'utda',
@@ -95,11 +90,6 @@ const vocab = [
 		def: 'to learn'
 	},
 	{
-		hangul: '공부하다',
-		romaji: 'gongbuhada',
-		def: 'to study'
-	},
-	{
 		hangul: '만나다',
 		romaji: 'mannada',
 		def: 'to meet'
@@ -125,6 +115,11 @@ const vocab = [
 		def: 'to be cold'
 	},
 	{
+		hangul: '행복하다',
+		romaji: 'haengboghada',
+		def: 'to be happy'
+	},
+	{
 		hangul: '슬프다',
 		romaji: 'seulpeuda',
 		def: 'to be sad'
@@ -139,7 +134,7 @@ const vocab = [
 		romaji: 'pigonhada',
 		def: 'to be tired'
 	},
-	{
+/* 	{
 		hangul: '재미있다',
 		romaji: 'jaemiitda',
 		def: 'to be interesting'
@@ -160,15 +155,10 @@ const vocab = [
 		def: 'to be easy'
 	},
 	{
-		hangul: '깨끗하다',
-		romaji: 'kkaekkeuthada',
-		def: 'to be clean'
-	},
-	{
 		hangul: '더럽다',
 		romaji: 'deoreopda',
 		def: 'to be dirty'
-	},
+	}, */
 	{
 		hangul: '빠르다',
 		romaji: 'ppareuda',
@@ -178,21 +168,156 @@ const vocab = [
 		hangul: '느리다',
 		romaji: 'neurida',
 		def: 'to be slow'
+	},
+	{
+		hangul: '예쁘다',
+		romaji: 'yeppeuda',
+		def: 'to be pretty'
+	},
+	{
+		hangul: '똑똑하다',
+		romaji: 'ttokttokhada',
+		def: 'to be smart'
+	},
+	{
+		hangul: '사다',
+		romaji: 'sada',
+		def: 'to buy'
+	},
+	{
+		hangul: '공부하다',
+		romaji: 'gongbuhada',
+		def: 'to study'
+	},
+	{
+		hangul: '깨끗하다',
+		romaji: 'kkaekkeuthada',
+		def: 'to be clean'
+	},
+	{
+		hangul: '사랑하다',
+		romaji: 'salanghada',
+		def: 'to love'
+	},
+	{
+		hangul: '놀다',
+		romaji: 'nolda',
+		def: 'to play'
+	},
+	{
+		hangul: '만들다',
+		romaji: 'mandeulda',
+		def: 'to make'
+	},
+	{
+		hangul: '작다',
+		romaji: 'jagda',
+		def: 'to be small'
+	},
+	{
+		hangul: '크다',
+		romaji: 'keuda',
+		def: 'to be big'
 	}
 ];
+
+const nounVocab = [
+	{
+		hangul: '오늘',
+		romaji: 'oneul',
+		def: 'today'
+	},
+	{
+		hangul: '내일',
+		romaji: 'naeil',
+		def: 'tomorrow'
+	},
+	{
+		hangul: '어제',
+		romaji: 'eoje',
+		def: 'yesterday'
+	},
+	{
+		hangul: '여기',
+		romaji: 'yeogi',
+		def: 'here'
+	},
+	{
+		hangul: '거기',
+		romaji: 'geogi',
+		def: 'there'
+	},
+	{
+		hangul: '날씨',
+		romaji: 'nalssi',
+		def: 'weather'
+	},
+	{
+		hangul: '책',
+		romaji: 'chaeg',
+		def: 'book'
+	},
+	{
+		hangul: '자동차',
+		romaji: 'jadongcha',
+		def: 'car'
+	},	
+	{ hangul: '몸', romaji: 'mom', def: 'body' },
+	{ hangul: '배', romaji: 'bae', def: 'stomach' },
+	{ hangul: '등', romaji: 'deung', def: 'back' },
+	{ hangul: '가슴', romaji: 'gaseum', def: 'chest' },
+	{ hangul: '이름', romaji: 'ileum', def: 'name' },
+	{ hangul: '도시', romaji: 'dosi', def: 'city' },
+	{ hangul: '셔츠', romaji: 'syeocheu', def: 'shirt' },
+	{ hangul: '바지', romaji: 'baji', def: 'pants' },
+		
+
+];
+
+const miscVocab = [
+	{ hangul: '아주', romaji: 'aju', def: 'very' },
+	{ hangul: '자주', romaji: 'jaju', def: 'frequently' },
+	{ hangul: '정말', romaji: 'jeongmal', def: 'really' },
+	{ hangul: '조금', romaji: 'jogeum', def: 'a little' },
+/* 	{ hangul: '꽤', romaji: 'kkwae', def: 'quite' },
+	{ hangul: '상당히', romaji: 'sangdanghi', def: 'quite' }, */
+	{ hangul: '거의', romaji: 'geoui', def: 'almost' },
+	{ hangul: '빨리', romaji: 'ppalli', def: 'fast' },
+	{ hangul: '천천히', romaji: 'cheoncheonhi', def: 'slowly' },
+	{ hangul: '신중하게', romaji: 'sinjunghage', def: 'carefully' },
+	{ hangul: '항상', romaji: 'hangsang', def: 'always' },
+	// { hangul: '늘', romaji: 'neul', def: 'always' },
+	{ hangul: '보통', romaji: 'botong', def: 'usually' },
+	{ hangul: '때로는', romaji: 'ttaeroneun', def: 'sometimes' },
+	// { hangul: '가끔', romaji: 'gakkeum', def: 'occasionally' },
+/* 	{ hangul: '좀처럼', romaji: 'jomcheoreom', def: 'seldom' },
+	{ hangul: '드물게', romaji: 'deumulge', def: 'rarely' }, */
+	{ hangul: '절대', romaji: 'jeoldae', def: 'never' },
+	{ hangul: '만나서 반가워요', romaji: 'Mannaseo bangawoeyo', def: 'Nice to meet you' },
+	{ hangul: '어떻게 지내세요?', romaji: 'Eotteoke jinaeseyo? ', def: 'How are you?' },
+	{ hangul: '잘 지내요 ', romaji: 'Jal jinaeyo', def: "I'm great" },
+	{ hangul: '저는 커피를 마시어요', romaji: 'naneun keopileul masieoyo', def: 'I drink coffee' },
+	{ hangul: '나는 여기에 자주 오요', romaji: 'naneun yeogie jaju oyo', def: 'I come here often' }
+]
+
+const vocab = [...primaryVocab,...nounVocab,...miscVocab]
+
+function randomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const MODE_TYPES = {
 	ASK_ENGLISH: { value: 0, text: 'Ask to type romaji from an English prompt' },
 	ASK_KOREAN: { value: 1, text: 'Ask what a romanized Korean word means' }
 };
 
-let curIndex = 0;
+let curIndex = randomInteger(0, vocab.length - 1);
 let mode = MODE_TYPES.ASK_KOREAN;
+let allAnswered = false;
 
 function sortVocab() {
 	vocab.sort(() => Math.random() - 0.5);
 }
-
 
 function changeMode() {
 	if (mode.value === MODE_TYPES.ASK_ENGLISH.value) {
@@ -214,45 +339,56 @@ function getQuestion() {
 	}
 }
 
-function processAskEnglish(userInput){
+function processAskEnglish(userInput) {
 	let expectedValueKey = 'romaji';
-	let statement = userInput === vocab[curIndex][expectedValueKey] ? 'Correct! ' : 'Incorrect... ';
+	let result = userInput === vocab[curIndex][expectedValueKey];
+	let statement = result ? 'Correct! ' : 'Incorrect... ';
 
 	document.getElementById(
 		'result'
 	).innerHTML = `Previous Result: ${statement} ${vocab[curIndex]['def']} is ${vocab[curIndex]['romaji']} (${vocab[curIndex]['hangul']})`;
 
+	return result;
 }
 
-function processAskKorean(userInput){
+function processAskKorean(userInput) {
 	let expectedValueKey = 'def';
-	let statement = userInput === vocab[curIndex][expectedValueKey] ? 'Correct! ' : 'Incorrect... ';
+	let result = userInput === vocab[curIndex][expectedValueKey];
+	let statement = result ? 'Correct! ' : 'Incorrect... ';
 
 	document.getElementById(
 		'result'
 	).innerHTML = `Previous Result: ${statement} ${vocab[curIndex]['romaji']} (${vocab[curIndex]['hangul']}) means ${vocab[curIndex]['def']}`;
 
+	return result;
 }
 
 function processInput() {
 	let userInput = document.getElementById('myText').value;
+	let answeredCorrect = false;
 	userInput = userInput.toLowerCase();
-	
-	if(mode.value === MODE_TYPES.ASK_ENGLISH.value){
-		processAskEnglish(userInput)
-	}
-	else {
-		processAskKorean(userInput)
+
+	if (mode.value === MODE_TYPES.ASK_ENGLISH.value) {
+		answeredCorrect = processAskEnglish(userInput);
+	} else {
+		answeredCorrect = processAskKorean(userInput);
 	}
 
-	if (curIndex === vocab.length - 1) curIndex = -1;
-	curIndex++;
-	getQuestion();
-	document.getElementById('myText').value = '';
+	if (answeredCorrect) vocab.splice(curIndex, 1);
+
+	curIndex = randomInteger(0, vocab.length - 1);
+	if (vocab.length < 1) allAnswered = true;
+
+	if (!allAnswered) {
+		getQuestion();
+		document.getElementById('myText').value = '';
+	} else {
+		document.getElementById('question').innerHTML = `You have answered all the questions!`;
+	}
 }
 
 document.getElementById('myText').addEventListener('keypress', function (e) {
-	if (e.key === 'Enter') {
+	if (e.key === 'Enter' && !allAnswered) {
 		processInput();
 	}
 });
